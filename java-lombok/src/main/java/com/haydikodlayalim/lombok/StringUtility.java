f399ff0207d72d1875b8fc7997bd0b06b1983291
Ferdi Firuz <58734461+ffiruz@users.noreply.github.com>
2022-07-06 09:21:31 +0300

    /@UtilityClass
    /@UtilityClass

--- java-lombok/src/main/java/com/haydikodlayalim/lombok/StringUtility.java
+++ java-lombok/src/main/java/com/haydikodlayalim/lombok/StringUtility.java
@@ -2,7 +2,7 @@ package com.haydikodlayalim.lombok;
 
 import lombok.experimental.UtilityClass;
 
-@UtilityClass
+@UtilityClass  //@UtilityClass annotation which creates a private constructor that throws an exception, makes the class final, and makes all methods static.
 public class StringUtility {
 
     public String trim(String value) {
